542acf4d73308d7645e33fe29dde1e3693c9c306
Eva Maréchal <evmarechal@gmail.com>
2020-12-09 16:24:52 +0100

    blue selection

--- controller/bluecontroller.php
+++ controller/bluecontroller.php
@@ -1,12 +1,13 @@
 <?php
 
-include "db.php";
+include "../model/db.php";
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 $query = 'SELECT * FROM films';
-$stmt = $dbh->exec($query);
+// execute sur la DB le query select from film
+$stmt = $dbh->query($query);
 try{
 
 //$connection = new \PDO($dsn, $username, $password);
@@ -17,23 +18,35 @@ try{
 }
 // On récupère tout le contenu de la table films
 
-
-
 // On affiche chaque entrée une à une
 while ($datas = $stmt->fetch())
 {
-?>
-    <?php echo $datas['name']; ?>
-    <?php echo $datas['director']; ?>
-    <?php echo $datas['actors']; ?>
-    <?php echo $datas['years']; ?> 
-    <?php echo $datas['synopsis']; ?>
-    <?php echo $datas['duration']; ?>
-    <?php echo $datas['poster']; ?>
-    <?php echo $datas['color']; ?>
-   </p>
-<?php
+    echo $datas['name']; 
+    echo $datas['director']; 
+    echo $datas['actors']; 
+    echo $datas['years']; 
+    echo $datas['synopsis'];
+    echo $datas['duration'];
+    echo $datas['poster']; 
+    echo $datas['color']; 
 }
-$reponse->closeCursor(); // Termine le traitement de la requête
 
-?>
+// On créé une requête query pour sélectionner seulement les films bleus 
+$query = "SELECT color, name FROM films WHERE color='blue'";
+// execution de la requête sur la DB
+$stmt = $dbh->query($query);
+
+while($result = $stmt->fetch())
+{
+    echo $result['name'];  
+}  
+
+// On créé une requête query pour sélectionner seulement les films bleus 
+$query = "SELECT color, name FROM films WHERE color='yellow'";
+// execution de la requête sur la DB
+$stmt = $dbh->query($query);
+
+while($result = $stmt->fetch())
+{
+    echo $result['name'];  
+}    
